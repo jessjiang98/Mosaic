efcf30642081cabceb46754169c82346c91c911b
jessjiang98 <jjiang9531@bths.edu>
2016-08-04 09:41:47 -0400

    create project

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function circle(x, y, diameter) {
+  ellipse (x, y, diameter);
+}
+
+function setup() {
+  createCanvas (640, 480);
+  stroke (255, 255, 255, 0);
+  fill (100, 15, 65);
+ circle (350, 350, 200);
+ fill (200, 60, 100);
+ circle (350, 350, 100);
+ fill (100, 15, 65);
+ circle (350, 350, 50);
+  
+}
+
+function draw() {
+
+}
